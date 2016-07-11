d6bcd64d769acf03e0baf8d9532428b2c82b30b7
taejoonJeon <taejjeon@gmail.com>
2016-07-11 18:33:07 +0900

    aa

--- app/src/main/java/com/hongik/alpha_money/MainActivity.java
+++ app/src/main/java/com/hongik/alpha_money/MainActivity.java
@@ -10,7 +10,7 @@ public class MainActivity extends AppCompatActivity {
         //heejung
 
 
-
+        //tj adadasd
         //branchwork....
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
