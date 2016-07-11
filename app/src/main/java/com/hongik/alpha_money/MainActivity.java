698c5726e4242c0731555d64d2f8975049d28094
Patabear <irr2000@paran.com>
2016-07-11 18:28:09 +0900

    branch work

--- app/src/main/java/com/hongik/alpha_money/MainActivity.java
+++ app/src/main/java/com/hongik/alpha_money/MainActivity.java
@@ -9,6 +9,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         //heejung
 
+
+
+        //branchwork....
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
