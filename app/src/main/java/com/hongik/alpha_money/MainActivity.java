0aacf2ebb8d9fc8ad1ab70df7c1d6d50235faca5
Patabear <irr2000@paran.com>
2016-07-11 18:34:28 +0900

    merge test

--- app/src/main/java/com/hongik/alpha_money/MainActivity.java
+++ app/src/main/java/com/hongik/alpha_money/MainActivity.java
@@ -9,8 +9,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         //heejung
 
-
-
+        //jhasjdhja
+        //ajhdujh
         //branchwork....
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
