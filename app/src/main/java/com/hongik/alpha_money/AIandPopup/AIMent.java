9e5b4e5b3e72ba276831d85343ffb9169326e07a
taejoonJeon <taejjeon@gmail.com>
2016-11-15 02:37:44 +0900

    AI Meant setting

---
+++ app/src/main/java/com/hongik/alpha_money/AIandPopup/AIMent.java
@@ -0,0 +1,23 @@
+package com.hongik.alpha_money.AIandPopup;
+
+import com.hongik.alpha_money.DataStructure.struct;
+
+/**
+ * Created by jeon3029 on 16. 11. 15..
+ */
+public class AIMent {
+    struct info = new struct();
+    public String getMent(struct st,int t){
+        info = st;
+        //나누어야 할 타입 : 시간, 카테고리
+        if(t == 0){//초록불
+
+        }
+        else if(t==1){//노란불
+
+        }
+        else if(t==2){//빨간불
+
+        }
+    }
+}
--- app/src/main/java/com/hongik/alpha_money/AIandPopup/PopupActivity.java
+++ app/src/main/java/com/hongik/alpha_money/AIandPopup/PopupActivity.java
@@ -8,14 +8,24 @@ import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import com.hongik.alpha_money.ApplicationSingleton;
+import com.hongik.alpha_money.DataStructure.SignalLight;
+import com.hongik.alpha_money.DataStructure.struct;
 import com.hongik.alpha_money.R;
+
+import org.w3c.dom.Text;
 
 public class PopupActivity extends Activity {
     RelativeLayout popupOutside;
     Button left,center,right;
+
+    ImageView signalLight;
+    TextView priceText,storeText,mentText;
+
     int option;
     String date;
     String price;
@@ -36,16 +46,9 @@ public class PopupActivity extends Activity {
         layoutParams.dimAmount = 0.7f;
         getWindow().setAttributes(layoutParams);
         setContentView(R.layout.popup_activity);
-        intent = getIntent();
-        date = intent.getStringExtra("date");
-        price = intent.getStringExtra("price");
-        storeName = intent.getStringExtra("storeName");
-        payment = intent.getStringExtra("payment");
-        //category = intent.getStringExtra("category");
-        //memo = intent.getStringExtra("memo");
-        gridX = intent.getDoubleExtra("gridX", 0.000000); // TODO : 0.000000으로 초기화해야함 테스트를 위해 수정했음
-        gridY = intent.getDoubleExtra("gridY", 0.000000);
-        //option = intent.getIntExtra("option", 0);
+
+        initiate();
+
 
         popupOutside = (RelativeLayout)this.findViewById(R.id.popup_outside);
         popupOutside.setOnClickListener(new View.OnClickListener() {
@@ -92,5 +95,56 @@ public class PopupActivity extends Activity {
             }
         });
 
+    }
+
+    private void initiate() {
+        intent = getIntent();
+        date = intent.getStringExtra("date");
+        price = intent.getStringExtra("price");
+        storeName = intent.getStringExtra("storeName");
+        payment = intent.getStringExtra("payment");
+
+
+        //category = intent.getStringExtra("category");
+        //memo = intent.getStringExtra("memo");
+        gridX = intent.getDoubleExtra("gridX", 0.000000); // TODO : 0.000000으로 초기화해야함 테스트를 위해 수정했음
+        gridY = intent.getDoubleExtra("gridY", 0.000000);
+        //option = intent.getIntExtra("option", 0);
+
+        signalLight = (ImageView)findViewById(R.id.popup_signal_image);
+        priceText = (TextView)findViewById(R.id.popup_price);
+        storeText = (TextView)findViewById(R.id.popup_storeName);
+        mentText = (TextView)findViewById(R.id.popup_ment);
+        priceText.setText(price + " 원");
+        storeText.setText(storeName);
+        setSignalLightImage();
+    }
+
+    private void setSignalLightImage() {
+        SignalLight sig = new SignalLight();
+        int t = sig.CalcSum();
+        t=t+Integer.parseInt(price);
+        int botLimit = ApplicationSingleton.getInstance().GetBotLimit();
+        int topLimit = ApplicationSingleton.getInstance().GetTopLimit();
+        struct temp = new struct();
+        temp.price = price;
+        temp.storeName =storeName;
+        temp.gridX = gridX;
+        temp.gridY = gridY;
+        temp.payment = payment;
+        temp.date =date;
+        AIMent ment = new AIMent();
+        if( t < botLimit){//green
+            signalLight.setImageResource(R.drawable.greencircle);
+            mentText.setText(ment.getMent(temp,0));
+        }
+        else if(t>=botLimit && t<topLimit){//yellow
+            signalLight.setImageResource(R.drawable.yellowcircle);
+            mentText.setText(ment.getMent(temp,1));
+        }
+        else{//red
+            signalLight.setImageResource(R.drawable.redcircle);
+            mentText.setText(ment.getMent(temp,2));
+        }
     }
 }
